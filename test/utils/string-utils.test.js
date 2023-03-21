e653ef30d7b7928f2754addbd10cb2ddb0dedd6b
Thiago Ferreira <thiagodnf@gmail.com>
2023-03-21 13:29:56 -0400

    some refactorings

--- test/utils/string-utils.test.js
+++ test/utils/string-utils.test.js
@@ -2,50 +2,52 @@ import StringUtils from "../../src/utils/string-utils";
 
 test("should be a valid string", async () => {
 
-    await expect(StringUtils.isString("")).toBe(true);
-    await expect(StringUtils.isString(" ")).toBe(true);
-    await expect(StringUtils.isString("   ")).toBe(true);
-    await expect(StringUtils.isString(" a")).toBe(true);
-    await expect(StringUtils.isString("a")).toBe(true);
+    expect(StringUtils.isString("")).toBe(true);
+    expect(StringUtils.isString(" ")).toBe(true);
+    expect(StringUtils.isString("   ")).toBe(true);
+    expect(StringUtils.isString(" a")).toBe(true);
+    expect(StringUtils.isString("a")).toBe(true);
 
-    await expect(StringUtils.isString(2)).toBe(false);
-    await expect(StringUtils.isString(2.3)).toBe(false);
-    await expect(StringUtils.isString(true)).toBe(false);
-    await expect(StringUtils.isString({})).toBe(false);
+    expect(StringUtils.isString(2)).toBe(false);
+    expect(StringUtils.isString(2.3)).toBe(false);
+    expect(StringUtils.isString(true)).toBe(false);
+    expect(StringUtils.isString({})).toBe(false);
 });
 
 test("should be a blank string", async () => {
 
-    await expect(StringUtils.isBlank("")).toBe(true);
-    await expect(StringUtils.isBlank(" ")).toBe(true);
-    await expect(StringUtils.isBlank("   ")).toBe(true);
-    await expect(StringUtils.isBlank()).toBe(true);
-    await expect(StringUtils.isBlank(null)).toBe(true);
-    await expect(StringUtils.isBlank(undefined)).toBe(true);
+    expect(StringUtils.isBlank("")).toBe(true);
+    expect(StringUtils.isBlank(" ")).toBe(true);
+    expect(StringUtils.isBlank("   ")).toBe(true);
+    expect(StringUtils.isBlank()).toBe(true);
+    expect(StringUtils.isBlank(null)).toBe(true);
+    expect(StringUtils.isBlank(undefined)).toBe(true);
+    expect(StringUtils.isBlank("\t\t\t    ")).toBe(true);
+    expect(StringUtils.isBlank("\n\n ")).toBe(true);
 
-    await expect(StringUtils.isBlank("a")).toBe(false);
-    await expect(StringUtils.isBlank("a ")).toBe(false);
-    await expect(StringUtils.isBlank(" a")).toBe(false);
-    await expect(StringUtils.isBlank(2)).toBe(false);
-    await expect(StringUtils.isBlank(2.3)).toBe(false);
-    await expect(StringUtils.isBlank(true)).toBe(false);
-    await expect(StringUtils.isBlank({})).toBe(false);
+    expect(StringUtils.isBlank("a")).toBe(false);
+    expect(StringUtils.isBlank("a ")).toBe(false);
+    expect(StringUtils.isBlank(" a")).toBe(false);
+    expect(StringUtils.isBlank(2)).toBe(false);
+    expect(StringUtils.isBlank(2.3)).toBe(false);
+    expect(StringUtils.isBlank(true)).toBe(false);
+    expect(StringUtils.isBlank({})).toBe(false);
 });
 
 test("should return a valid json object from json content", async () => {
 
-    await expect(StringUtils.parseJson("fake.json", "{}")).toEqual({});
-    await expect(StringUtils.parseJson("fake.json", "{\"one\": 1, \"two\": 2}")).toEqual({one: 1, two: 2});
+    expect(StringUtils.parseJson("fake.json", "{}")).toStrictEqual({});
+    expect(StringUtils.parseJson("fake.json", "{\"one\": 1, \"two\": 2}")).toEqual({one: 1, two: 2});
 
-    await expect(() => StringUtils.parseJson("fake.json", "")).toThrow(Error);
-    await expect(() => StringUtils.parseJson("fake.json", "  ")).toThrow(Error);
-    await expect(() => StringUtils.parseJson("fake.json", "{one: 1, two: 2}")).toThrow(Error);
+    expect(() => StringUtils.parseJson("fake.json", "")).toThrow(Error);
+    expect(() => StringUtils.parseJson("fake.json", "  ")).toThrow(Error);
+    expect(() => StringUtils.parseJson("fake.json", "{one: 1, two: 2}")).toThrow(Error);
 });
 
 test("should return a valid yaml object from json content", async () => {
 
-    await expect(StringUtils.parseYaml("fake.yaml", "foo:bar")).not.toBe(null);
+    expect(StringUtils.parseYaml("fake.yaml", "foo: bar")).toStrictEqual({"foo": "bar"});
 
-    await expect(() => StringUtils.parseYaml("fake.yaml", "")).toThrow(Error);
-    await expect(() => StringUtils.parseYaml("fake.yaml", "  ")).toThrow(Error);
+    expect(() => StringUtils.parseYaml("fake.yaml", "")).toThrow(Error);
+    expect(() => StringUtils.parseYaml("fake.yaml", "  ")).toThrow(Error);
 });
